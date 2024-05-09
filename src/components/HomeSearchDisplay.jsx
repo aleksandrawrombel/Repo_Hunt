1b31488c932c29e61e10c51cd87fb36bc470d988
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-05-09 12:02:18 +0200

    API search results logging correclty, starting on displaying the information

--- src/components/HomeSearchDisplay.jsx
+++ src/components/HomeSearchDisplay.jsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
+
 const HomeSearchDisplay = () => {
+    
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://api.github.com/search/repositories?q=${query}`
+      );
+      const data = await response.json();
+      setSearchResults(data);
+      console.log(searchResults);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <main className="container">
       <section className="search_container">
-        <input className="search_bar" type="text" />
-        <button className="search_button">
+        <input
+          className="search_bar"
+          placeholder="Search..."
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="search_button" onClick={handleSearch}>
           <span className="search_emoji">&#128269;</span> Szukaj
         </button>
+      </section>
+      <section className="results_container">
+        
       </section>
     </main>
   );
