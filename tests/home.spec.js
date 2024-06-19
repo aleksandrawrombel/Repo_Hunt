df86fad5e5b915a6ce2b31dcaebf0ebfa8814b2f
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-06-19 12:26:06 +0200

    selenium setup added to the project, first test added and running correctly

---
+++ tests/home.spec.js
@@ -0,0 +1,22 @@
+import { Builder, Browser } from "selenium-webdriver";
+import assert from 'assert';
+
+async function homeTest() {
+  let driver;
+
+  try {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get("http://localhost:5173/");
+
+    let title = await driver.getTitle();
+    assert.equal("Reposzukiwacz", title);
+
+    console.log('test running correctly');
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await driver.quit();
+  }
+}
+
+homeTest();
